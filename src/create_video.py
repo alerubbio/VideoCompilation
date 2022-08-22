3721e3850f9f1c0aaba099d323678b2c1e0110c9
Alejandro Rubio <rubio.alejandro7@gmail.com>
2022-08-22 12:10:06 -0400

    Update create_video.py
    - attempting to debug, not sure why it works when explicitly created but not when using a loop to make a list

--- src/create_video.py
+++ src/create_video.py
@@ -1,11 +1,8 @@
-# read in list of valid clips
-# concat them
-# edit them
-
+import os
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 
 PATH_TO_VALID_CLIPS = 'VideoCompilation/ClipData/valid_clips.txt'
-PATH_TO_RAW_CLIPS = 'VideoCompilation\VideoFiles\\raw_clips\\'
+PATH_TO_RAW_CLIPS = 'E:\Projects\TwitchMontage\VideoCompilation\VideoFiles\\raw_clips'
 
 def read_valid_clips_list():
     #read valid clips
@@ -15,18 +12,24 @@ def read_valid_clips_list():
 
 def create_clips_from_list(list):
     clips = []
-    for item in list:
-        clip = VideoFileClip(str(PATH_TO_RAW_CLIPS) + str(item))
+    for filename in list:
+        video_file_path = os.path.abspath(os.path.join(PATH_TO_RAW_CLIPS, filename))
+        clip = VideoFileClip(str(video_file_path))
         clips.append(clip)
 
     return clips
-
 
 def create_draft(clips):
     draft = concatenate_videoclips(clips)
     draft.write_videofile("VideoCompilation/VideoFiles/videos/draft.mp4")
     return draft
 
-list = read_valid_clips_list()
-clips = create_clips_from_list(list)
-draft = create_draft(clips)
+# list = read_valid_clips_list()
+# clips = create_clips_from_list(list)
+# draft = create_draft(clips)
+
+
+clip1 = VideoFileClip('VideoCompilation\VideoFiles\\raw_clips\clip0.mp4')
+clip2 = VideoFileClip('VideoCompilation\VideoFiles\\raw_clips\clip1.mp4')
+combined = concatenate_videoclips([clip1, clip2])
+combined.write_videofile("VideoCompilation/VideoFiles/videos/draft.mp4")
