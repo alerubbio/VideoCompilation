bd752aa460e9e7ee660730e11ff63bb4025f6a9b
Alejandro Rubio <rubio.alejandro7@gmail.com>
2022-08-24 17:13:34 -0400

    installed font and fixed tuple error

--- src/create_video.py
+++ src/create_video.py
@@ -1,21 +1,32 @@
 import os
-from moviepy.editor import VideoFileClip, concatenate_videoclips
+import json
+from moviepy.editor import VideoFileClip, CompositeVideoClip, concatenate_videoclips, vfx, TextClip
 
 PATH_TO_VALID_CLIPS = 'VideoCompilation/ClipData/valid_clips.txt'
-PATH_TO_RAW_CLIPS = 'VideoCompilation\VideoFiles\/raw_clips'
-
+PATH_TO_RAW_CLIPS = 'VideoCompilation\VideoFiles\\raw_clips'
+FONT_PATH = 'E:\Projects\TwitchMontage\VideoCompilation\Fonts\Twitchy.TV Font\Twitchy.TV.ttf'
 def read_valid_clips_list():
     #read valid clips
     file = open(PATH_TO_VALID_CLIPS, 'r')
     list = file.readlines()
     return list
+    
+def get_clip_data(filename):
+    file = open('VideoCompilation\ClipData\clips_data.json')
+    clips_data = json.load(file)
 
-def create_clips_from_list(list):
+    return clips_data[filename]
+
+def create_clips(list):
     clips = []
     for filename in list:
-        video_file_path = os.path.join(PATH_TO_RAW_CLIPS, filename.strip())
+        filename = filename.strip()
+        video_file_path = os.path.join(PATH_TO_RAW_CLIPS, filename)
 
         clip = VideoFileClip(video_file_path)
+        clip = clip.fx(vfx.fadein, .1).fx(vfx.fadeout, .15)
+        clip = add_text_overlay(clip, filename)
+
         clips.append(clip)
 
     return clips
@@ -25,6 +36,26 @@ def create_draft(clips):
     draft.write_videofile("VideoCompilation/VideoFiles/videos/draft.mp4")
     return draft
 
-list = read_valid_clips_list()
-clips = create_clips_from_list(list)
-draft = create_draft(clips)
+def add_text_overlay(clip, file):
+    clip_data = get_clip_data(file)
+    streamerName = str(clip_data.get('streamerName'))
+
+    text_clip = TextClip(txt = streamerName, size = (400,0), color = "blue")
+    tc_width, tc_height = text_clip.size
+
+    text_clip = text_clip.set_position(0, tc_height)
+    text_clip = text_clip.set_start((0,0))
+    text_clip = text_clip.set_duration(2.5)
+    text_clip = text_clip.crossfadein(0.5).crossfadeout(0.5)
+    clip = CompositeVideoClip([clip, text_clip])
+    return clip
+
+def create_movie():
+    list = read_valid_clips_list()
+    clips = create_clips(list)
+    draft = create_draft(clips)
+
+    return draft
+    
+movie = create_movie()
+
