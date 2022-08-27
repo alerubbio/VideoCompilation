c1756dd1a6781afebcd044815f8ba218f3de1e0e
Alejandro Rubio <rubio.alejandro7@gmail.com>
2022-08-27 15:31:08 -0400

    Attempting to concatenate by adding text overlay
    This version currently creates video but only adds the first text overlay for 'shroud'.
    
    In comments is another version that could work but it doesn't position text correctly and it makes the text play back to back.

--- src/create_video.py
+++ src/create_video.py
@@ -5,57 +5,78 @@ from moviepy.editor import VideoFileClip, CompositeVideoClip, concatenate_videoc
 PATH_TO_VALID_CLIPS = 'VideoCompilation/ClipData/valid_clips.txt'
 PATH_TO_RAW_CLIPS = 'VideoCompilation\VideoFiles\\raw_clips'
 FONT_PATH = 'E:\Projects\TwitchMontage\VideoCompilation\Fonts\Twitchy.TV Font\Twitchy.TV.ttf'
+
+# reads clip valid clip names from file
 def read_valid_clips_list():
     #read valid clips
     file = open(PATH_TO_VALID_CLIPS, 'r')
-    list = file.readlines()
-    return list
-    
+    valid_list = file.readlines()
+    return valid_list
+
+# gets clip data
 def get_clip_data(filename):
     file = open('VideoCompilation\ClipData\clips_data.json')
     clips_data = json.load(file)
 
     return clips_data[filename]
 
-def create_clips(list):
+def create_clips(clip_list):
     clips = []
-    for filename in list:
+    texts = []
+    currentTotalDuration = 0
+    for filename in clip_list:
         filename = filename.strip()
+        clip_data = get_clip_data(filename)
         video_file_path = os.path.join(PATH_TO_RAW_CLIPS, filename)
 
+        current_clip_duration = float(clip_data['clipDuration'])
+        
+        # create video clip
         clip = VideoFileClip(video_file_path)
+        clip = clip.set_duration(current_clip_duration)
         clip = clip.fx(vfx.fadein, .1).fx(vfx.fadeout, .15)
-        clip = add_text_overlay(clip, filename)
 
+        # create text overlay for clip
+        text = create_text_overlay(clip_data, currentTotalDuration)
+        
+        # combine clip and text before concatenation
+        clip = CompositeVideoClip([clip, text]).set_duration(current_clip_duration) #
+        currentTotalDuration += current_clip_duration
+
+        texts.append(text)
         clips.append(clip)
 
-    return clips
+    return clips, texts, currentTotalDuration
 
-def create_draft(clips):
-    draft = concatenate_videoclips(clips, method='compose')
-    draft.write_videofile("VideoCompilation/VideoFiles/videos/draft.mp4")
-    return draft
-
-def add_text_overlay(clip, file):
-    clip_data = get_clip_data(file)
+def create_text_overlay(clip_data, currentDuration):
     streamerName = str(clip_data.get('streamerName'))
 
-    text_clip = TextClip(txt = streamerName, size = (400,0), color = "blue")
+    text_clip = TextClip(txt = streamerName, font = FONT_PATH, size = (400,0), color = 'rgb(145, 70, 255)')
     tc_width, tc_height = text_clip.size
 
-    text_clip = text_clip.set_position(0, tc_height)
-    text_clip = text_clip.set_start((0,0))
+    print(currentDuration)
+    text_clip = text_clip.set_start(currentDuration)
+    text_clip = text_clip.set_position(('left', 'bottom'))
     text_clip = text_clip.set_duration(2.5)
-    text_clip = text_clip.crossfadein(0.5).crossfadeout(0.5)
-    clip = CompositeVideoClip([clip, text_clip])
-    return clip
+    text_clip = text_clip.crossfadein(0.2).crossfadeout(0.5)
+
+    return text_clip
+
+def create_final_video(clips, texts, totalDuration):
+    vid_clips = concatenate_videoclips(clips, method='compose').set_duration(totalDuration)
+    # print(type(vid_clips))
+    # text_clips = concatenate_videoclips(texts).set_duration(totalDuration)
+    # print(type(text_clips))
+
+    # final_movie = CompositeVideoClip([vid_clips, text_clips], size=(1920,1080)).set_duration(totalDuration)
+    return vid_clips
 
 def create_movie():
-    list = read_valid_clips_list()
-    clips = create_clips(list)
-    draft = create_draft(clips)
-
-    return draft
+    valid_list = read_valid_clips_list()
+    clips, texts, totalDuration = create_clips(valid_list)
+    movie = create_final_video(clips, texts, totalDuration)
     
-movie = create_movie()
+    return movie
 
+movie = create_movie()
+movie.write_videofile('VideoCompilation\VideoFiles\\videos\movie.mp4')
